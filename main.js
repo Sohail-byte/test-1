f9384b647fb399c91772a2719ac52b7b34cffca1
Your Name <you@example.com>
2024-07-22 18:54:37 +0500

    updated the projects page, added projects images

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+
 import './styles/style.css'
 import './styles/background.css'
 import './styles/nav-bar.css'
 import './styles/home-page.css'
 import './styles/projects.css'
-
 
 const navBtn = document.querySelector('.menu')
 const navBar = document.querySelector('.small-menu')
@@ -12,7 +12,4 @@ const navBar = document.querySelector('.small-menu')
 navBtn.addEventListener('click', function(e){
  navBar.classList.toggle('open')
 })
-
-
-
 
